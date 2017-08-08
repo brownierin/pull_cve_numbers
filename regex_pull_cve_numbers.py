a7cd937ddf0c7138a33750d99b65d58b3082165e
Erin Browning <erin_l_browning@bcbsil.com>
2017-08-08 15:29:06 -0500

    Pulls CVE numbers with regexes

---
+++ regex_pull_cve_numbers.py
@@ -0,0 +1,35 @@
+import requests
+import re
+
+def get_page(url):
+	page = requests.get(url)
+	if page.text:
+		print "page retrieved"
+		return page.text
+	else:
+		print "Problem downloading page"
+
+def remove_duplicates(cves):
+	unique_cves = []
+	for cve in cves:
+		if cve not in unique_cves:
+			unique_cves.append(cve)
+	return unique_cves
+
+def prettyprint(cves):
+	for cve in cves:
+		print cve
+	print "Number of CVEs found: " + str(len(cves))
+	return cves
+
+def process():
+	url = raw_input("Input a URL: ")
+	page = get_page(url)
+	cves = re.findall(r'CVE-\d{4}-\d{4,6}', page)
+	if cves:
+		cves = remove_duplicates(cves)
+		prettyprint(cves)
+	else:
+		print "No CVEs found"
+
+process()
