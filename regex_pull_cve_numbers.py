7a9a433043744d9973a7f89c902cacb624d84b68
Erin Browning <erin_l_browning@bcbsil.com>
2017-08-24 14:19:02 -0500

    turn tabs to spaces

--- regex_pull_cve_numbers.py
+++ regex_pull_cve_numbers.py
@@ -3,38 +3,38 @@ import re
 import argparse
 
 def get_page(url):
-	try: 
-		page = requests.get(url)
-		print "[+] Page retrieved successfully"
-		return page.text
-	except Exception:
-		print "[-] Problem downloading page"
+    try: 
+        page = requests.get(url)
+        print "[+] Page retrieved successfully"
+        return page.text
+    except Exception:
+        print "[-] Problem downloading page"
 
 def get_file(filename):
-	try:
-		return open(filename, "r").read()
-	except Exception:
-		print "[-] Problem opening file"
+    try:
+        return open(filename, "r").read()
+    except Exception:
+        print "[-] Problem opening file"
 
 def prettyprint(cves):
-	print "[+] Listing unique CVEs"
-	for cve in cves:
-		print cve
-	print "[+] Number of CVEs found: {}".format(len(cves))
+    print "[+] Listing unique CVEs"
+    for cve in cves:
+        print cve
+    print "[+] Number of CVEs found: {}".format(len(cves))
 
 def process():
-	if args.file:
-		text = get_file(args.file)
-	elif args.url:
-		text = get_page(args.url)
-	cves = re.findall(r'CVE-\d{4}-\d{4,6}', text)
-	if cves:
-		prettyprint(set(cves))
-	else:
-		print "[-] No CVEs found"
-	
+    if args.file:
+        text = get_file(args.file)
+    elif args.url:
+        text = get_page(args.url)
+    cves = re.findall(r'CVE-\d{4}-\d{4,6}', text)
+    if cves:
+        prettyprint(set(cves))
+    else:
+        print "[-] No CVEs found"
+    
 parser = argparse.ArgumentParser(description='Pulls CVEs from either a \
-	page or from a file')
+    page or from a file')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('--url', '-u',
                 help='the url of the page')
